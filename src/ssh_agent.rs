7aea5278b0e964ebf8450ccf857781d6a8e74690
Rustafarian Dev <rustafarian.dev@gmail.com>
2023-07-24 03:17:16 +0000

    General Maintenance
    Format
    Generalize the parsing of SshSigingKeys
    Add Constraint parsing

--- src/ssh_agent.rs
+++ src/ssh_agent.rs
@@ -1,61 +1,88 @@
-use crate::message_builder::*;
-use crate::ssh_agent_types::*;
 use crate::data_reader::*;
-use crate::error::*;
+use crate::dsa_key::DsaKey;
 use crate::ecdsa_key::EcDsaKey;
-
+use crate::ed25519_key::Ed25519Key;
+use crate::error::*;
+use crate::message_builder::*;
+use crate::rsa_key::RsaKey;
+use crate::ssh_agent_types::*;
+use std::time::Duration;
 use zeroize::{Zeroize, ZeroizeOnDrop};
 #[derive(Zeroize, ZeroizeOnDrop, Debug)]
 pub struct SafeBytes(pub Vec<u8>);
 
+enum Constraint {
+    MaxTime(std::time::Instant),
+    Confirm,
+    MaxSign(u32),
+}
+
+impl TryFromDataReader for Vec<Constraint> {
+    fn try_from_data_reader(r: &mut DataReader<'_>) -> Result<Vec<Constraint>> {
+        use ConstraintType::*;
+        let mut contraints = Vec::new();
+        while r.more() {
+            match ConstraintType::try_from(r.get_u8()?).map_err(|_| ErrorKind::Unsupported)? {
+                Lifetime => {
+                    let dur = Duration::from_secs(r.get_u32()? as u64);
+                    let t = std::time::Instant::now()
+                        .checked_add(dur)
+                        .ok_or(ErrorKind::Parse)?;
+                    contraints.push(Constraint::MaxTime(t));
+                }
+                Confirm => {
+                    contraints.push(Constraint::Confirm);
+                }
+                Maxsign => {
+                    contraints.push(Constraint::MaxSign(r.get_u32()?));
+                }
+                Extension => {
+                    return Err(ErrorKind::Unsupported);
+                }
+            }
+        }
+        Ok(contraints)
+    }
+}
+
 enum LockStatus {
-	Locked,
-	Unlocked,
+    Locked,
+    Unlocked,
 }
 
 pub struct SshAgent {
-	lock_status: LockStatus,
+    lock_status: LockStatus,
     secret: SafeBytes,
     identities: Vec<SshIdentity>,
 }
 
 pub trait SshSigningKey {
-    fn sign(&self, id:&SshIdentity, data: &[u8], _flags: u32) -> Result<Vec<u8>>;
-    fn public_key(&self) -> Result<Vec<u8>>;
-    fn matches(&self, key: &[u8]) -> bool {
-        let mut reader = DataReader::new(key);
-        let _key_type = match reader.get_slice() {
-            Ok(v) => v,
-            Err(_) => return false,
-        };
-        let given_public = match reader.get_slice() {
-            Ok(v) => v,
-            Err(_) => return false,
-        };
-        let public = match self.public_key() {
-            Ok(v) => v,
-            Err(_) => return false,
-        };
-
-        given_public == public
-    }
+    fn sign(&self, id: &SshIdentity, data: &[u8], _flags: u32) -> Result<Vec<u8>>;
+    fn id(&self) -> &[u8];
 }
 
-pub struct Ed25519Key (ed25519_dalek::Keypair);
+pub struct SshSigningKeyInfo<T: SshSigningKey> {
+    key: T,
+    comment: Vec<u8>,
+    id: Vec<u8>,
+    constraints: Vec<Constraint>,
+}
 
-impl SshSigningKey for Ed25519Key {
-    fn sign(&self, _: &SshIdentity, data: &[u8], _: u32) -> Result<Vec<u8>> {
-        use ed25519_dalek::{Signer};
-        Ok(
-            append_parts(&[
-                "ssh-ed25519".as_bytes(),
-                self.0.sign(data).as_ref(),
-            ])
-        )
-    }
-
-    fn public_key(&self) -> Result<Vec<u8>> {
-        Ok(self.0.public.as_bytes().to_vec())
+impl<T> TryFromDataReader for SshSigningKeyInfo<T>
+where
+    T: TryFromDataReader + SshSigningKey,
+{
+    fn try_from_data_reader(r: &mut DataReader<'_>) -> Result<Self> {
+        let key = T::try_from_data_reader(r)?;
+        let id = key.id().to_vec();
+        let comment = r.get_slice()?.to_vec();
+        let constraints = <Vec<Constraint>>::try_from_data_reader(r)?;
+        Ok(Self {
+            key,
+            comment,
+            id,
+            constraints,
+        })
     }
 }
 
@@ -63,87 +90,77 @@ pub enum SshKey {
     Ed25519Key(Ed25519Key),
     EcDsaNistP256(EcDsaKey<p256::NistP256>),
     EcDsaNistP384(EcDsaKey<p384::NistP384>),
+    RsaKey(RsaKey),
+    DsaKey(DsaKey),
 }
 
 pub struct SshIdentity {
     key_type: Vec<u8>,
     comment: Vec<u8>,
     key: SshKey,
+    constraints: Vec<Constraint>,
 }
 
-pub fn add_u32_to_vec(vec:&mut Vec<u8>, num:u32) {
-    vec.push(((num & 0xFF000000) >> 24) as u8);
-    vec.push(((num & 0x00FF0000) >> 16) as u8);
-    vec.push(((num & 0x0000FF00) >> 8) as u8);
-    vec.push((num & 0x000000FF) as u8);
-}
-
-pub fn append_parts(parts: &[&[u8]]) -> Vec<u8> {
-    let len = parts.iter().map(|e| e.len() + 4).sum::<usize>();
-    let mut data: Vec<u8> = Vec::with_capacity(len);
-    for part in parts {
-        add_u32_to_vec(&mut data, part.len() as u32);
-        data.extend_from_slice(part);
-    }
-    data
+struct IdentityItem<'a> {
+    id: &'a [u8],
+    comment: &'a [u8],
 }
 
 impl SshIdentity {
-    pub fn from_bytes(data:&[u8]) -> Result<SshIdentity> {
-        let mut reader = DataReader::with_pos(1, data);
-        let key_type = reader.get_slice()?;
-        let key_type_str = std::str::from_utf8(key_type)?;
-        match key_type_str {
-            "ssh-ed25519" => {
-                let public = reader.get_slice()?;
-                let private = reader.get_slice()?;
-                let comment = reader.get_slice()?;
-                Ok(SshIdentity{
-                    key_type: append_parts(&[key_type, public]),
-                    key: SshKey::Ed25519Key(
-                        Ed25519Key(
-                            ed25519_dalek::Keypair::from_bytes(private).map_err(|_| ErrorKind::KeyNotCreated)?
-                        )
-                    ),
-                    comment: comment.to_vec(),
+    pub fn from_bytes(data: &[u8]) -> Result<SshIdentity> {
+        let mut reader = DataReader::new(data);
+        reader.skip_u8()?;
+        let key_type = reader.peek_slice()?;
+        match key_type {
+            b"ssh-ed25519" => {
+                let info = SshSigningKeyInfo::<Ed25519Key>::try_from_data_reader(&mut reader)?;
+                Ok(SshIdentity {
+                    key_type: info.id,
+                    key: SshKey::Ed25519Key(info.key),
+                    comment: info.comment,
+                    constraints: info.constraints,
                 })
-            },
-            "ecdsa-sha2-nistp256" => {
-                let curve_name = reader.get_slice()?;
-                let q = reader.get_slice()?;
-                let d = reader.get_slice()?;
-                let private = if d[0] == 0 { &d[1..] } else {d };
-                let comment = reader.get_slice()?;
-                Ok(SshIdentity{
-                    key_type: append_parts(&[key_type, curve_name, q]),
-                    key: SshKey::EcDsaNistP256(
-                        EcDsaKey::new(
-                           p256::ecdsa::SigningKey::from_slice(private).map_err(|_| ErrorKind::KeyNotCreated)?,
-                           key_type.to_vec(),
-                           curve_name.to_vec(),
-                        ),
-                    ),
-                    comment: comment.to_vec(),
+            }
+            b"ecdsa-sha2-nistp256" => {
+                let info = SshSigningKeyInfo::<EcDsaKey<p256::NistP256>>::try_from_data_reader(
+                    &mut reader,
+                )?;
+                Ok(SshIdentity {
+                    key_type: info.id,
+                    key: SshKey::EcDsaNistP256(info.key),
+                    comment: info.comment,
+                    constraints: info.constraints,
                 })
-            },
-            "ecdsa-sha2-nistp384" => {
-                let curve_name = reader.get_slice()?;
-                let q = reader.get_slice()?;
-                let d = reader.get_slice()?;
-                let private = if d[0] == 0 { &d[1..] } else {d };
-                let comment = reader.get_slice()?;
-                Ok(SshIdentity{
-                    key_type: append_parts(&[key_type, curve_name, q]),
-                    key: SshKey::EcDsaNistP384(
-                        EcDsaKey::new(
-                           p384::ecdsa::SigningKey::from_slice(private).map_err(|_| ErrorKind::KeyNotCreated)?,
-                           key_type.to_vec(),
-                            curve_name.to_vec(),
-                        ),
-                    ),
-                    comment: comment.to_vec(),
+            }
+            b"ecdsa-sha2-nistp384" => {
+                let info = SshSigningKeyInfo::<EcDsaKey<p384::NistP384>>::try_from_data_reader(
+                    &mut reader,
+                )?;
+                Ok(SshIdentity {
+                    key_type: info.id,
+                    key: SshKey::EcDsaNistP384(info.key),
+                    comment: info.comment,
+                    constraints: info.constraints,
                 })
-            },
+            }
+            b"ssh-rsa" => {
+                let info = SshSigningKeyInfo::<RsaKey>::try_from_data_reader(&mut reader)?;
+                Ok(SshIdentity {
+                    key_type: info.id,
+                    key: SshKey::RsaKey(info.key),
+                    comment: info.comment,
+                    constraints: info.constraints,
+                })
+            }
+            b"ssh-dss" => {
+                let info = SshSigningKeyInfo::<DsaKey>::try_from_data_reader(&mut reader)?;
+                Ok(SshIdentity {
+                    key_type: info.id,
+                    key: SshKey::DsaKey(info.key),
+                    comment: info.comment,
+                    constraints: info.constraints,
+                })
+            }
             _ => Err(ErrorKind::TypeNotFound),
         }
     }
@@ -153,23 +170,8 @@ impl SshIdentity {
             SshKey::Ed25519Key(key) => key.sign(self, data, _flags),
             SshKey::EcDsaNistP256(key) => key.sign(self, data, _flags),
             SshKey::EcDsaNistP384(key) => key.sign(self, data, _flags),
-        }
-    }
-
-    fn matches(&self, blob: &[u8]) -> bool {
-        match &self.key {
-            SshKey::Ed25519Key(key) => key.matches(blob),
-            SshKey::EcDsaNistP256(key) => key.matches(blob),
-            SshKey::EcDsaNistP384(key) => key.matches(blob),
-        }
-    }
-
-    fn public_key(&self) -> Result<Vec<u8>> {
-        match &self.key {
-            SshKey::Ed25519Key(key) => key.public_key(),
-            SshKey::EcDsaNistP256(key) => key.public_key(),
-            SshKey::EcDsaNistP384(key) => key.public_key(),
-
+            SshKey::RsaKey(key) => key.sign(self, data, _flags),
+            SshKey::DsaKey(key) => key.sign(self, data, _flags),
         }
     }
 }
@@ -185,23 +187,19 @@ impl SshAgent {
 
     pub fn handle_msg(&mut self, data: &[u8]) -> Result<MessageBuilder> {
         match self.lock_status {
-            LockStatus::Unlocked => {
-                self.handle_msg_in_unlocked(data)
-            },
-            LockStatus::Locked => {
-                self.handle_msg_in_locked(data)
-            },
+            LockStatus::Unlocked => self.handle_msg_in_unlocked(data),
+            LockStatus::Locked => self.handle_msg_in_locked(data),
         }
     }
 
     fn handle_msg_in_locked(&mut self, data: &[u8]) -> Result<MessageBuilder> {
         use SshAgentRequestType::*;
-        let msg_type = data.get(0).ok_or(ErrorKind::TooShort)?;
+        let msg_type = data.first().ok_or(ErrorKind::TooShort)?;
         let t = SshAgentRequestType::try_from(*msg_type).map_err(|_| ErrorKind::TooShort)?;
         match t {
-                Unlock => self.unlock(data),
-                RequestIdentities => self.empty_identities(),
-                _ => Ok(MessageBuilder::failure())
+            Unlock => self.unlock(data),
+            RequestIdentities => self.empty_identities(),
+            _ => Ok(MessageBuilder::failure()),
         }
     }
 
@@ -212,7 +210,8 @@ impl SshAgent {
     }
 
     fn sign_request(&self, data: &[u8]) -> Result<MessageBuilder> {
-        let mut reader = DataReader::with_pos(1, data);
+        let mut reader = DataReader::new(data);
+        reader.skip_u8()?;
         let key = reader.get_slice()?;
         let to_sign = reader.get_slice()?;
         let flags = reader.get_u32()?;
@@ -225,19 +224,34 @@ impl SshAgent {
     }
 
     fn find_identity(&self, key: &[u8]) -> Result<&SshIdentity> {
-        self.identities.iter().find(|&e| e.matches(key)).ok_or(ErrorKind::KeyNotFound)
+        self.identities
+            .iter()
+            .find(|&e| e.key_type == key)
+            .ok_or(ErrorKind::KeyNotFound)
     }
 
     fn find_position(&self, key: &[u8]) -> Result<usize> {
-        self.identities.iter().position(|e| e.matches(key)).ok_or(ErrorKind::KeyNotFound)
+        self.identities
+            .iter()
+            .position(|e| e.key_type == key)
+            .ok_or(ErrorKind::KeyNotFound)
     }
 
     fn list_identities(&mut self) -> Result<MessageBuilder> {
         let mut msg = MessageBuilder::new(SshAgentResponseType::IdentitiesAnswer as u8);
-        msg.add_u32(self.identities.len() as u32);
-        for identity in &self.identities {
-            msg.add_bytes(&identity.key_type);
-            msg.add_str(std::str::from_utf8(&identity.comment).unwrap());
+        let list: Vec<IdentityItem<'_>> = self
+            .identities
+            .iter()
+            .filter(|i| true)
+            .map(|i| IdentityItem {
+                id: &i.key_type,
+                comment: &i.comment,
+            })
+            .collect();
+        msg.add_u32(list.len() as u32);
+        for i in list {
+            msg.add_bytes(i.id);
+            msg.add_bytes(i.comment);
         }
 
         Ok(msg)
@@ -245,12 +259,11 @@ impl SshAgent {
 
     fn parse_identity(&mut self, data: &[u8]) -> Result<SshIdentity> {
         SshIdentity::from_bytes(data)
-
     }
 
     fn add_identity(&mut self, data: &[u8]) -> Result<MessageBuilder> {
-       let identity = self.parse_identity(data)?;
-       self.identities.push(identity);
+        let identity = self.parse_identity(data)?;
+        self.identities.push(identity);
         Ok(MessageBuilder::success())
     }
 
@@ -261,28 +274,55 @@ impl SshAgent {
 
     fn handle_msg_in_unlocked(&mut self, data: &[u8]) -> Result<MessageBuilder> {
         use SshAgentRequestType::*;
-        let msg_type = data.get(0).ok_or(ErrorKind::TooShort)?;
-
-        println!("type {}", msg_type);
+        let msg_type = data.first().ok_or(ErrorKind::TooShort)?;
         let t = SshAgentRequestType::try_from(*msg_type).map_err(|_| ErrorKind::BadRequestType)?;
         match t {
-            Lock =>  self.lock(data),
+            Lock => self.lock(data),
             RequestIdentities => self.list_identities(),
             RemoveAllIdentities => self.remove_identities(),
             RemoveIdentity => self.remove_identity(data),
             AddIdentity => self.add_identity(data),
             SignRequest => self.sign_request(data),
-            Extension => Ok(MessageBuilder::new(SshAgentResponseType::ExtensionFailure as u8)),
-            _ => self.unhandled(data)
+            Extension => self.extension(data),
+            _ => self.unhandled(data),
         }
     }
 
-    fn unhandled(&mut self, _:&[u8]) -> Result<MessageBuilder> {
+    fn extension(&mut self, data: &[u8]) -> Result<MessageBuilder> {
+        let mut reader = DataReader::new(data);
+        reader.skip_u8()?;
+        let extension = reader.get_slice()?;
+        match extension {
+            b"query" => {
+                let mut msg = MessageBuilder::success();
+                msg.add_str("query");
+                msg.add_str("session-bind@openssh.com");
+                Ok(msg)
+            }
+            b"session-bind@openssh.com" => self.openssh_com_session_bind(&mut reader),
+            _ => Ok(MessageBuilder::new(
+                SshAgentResponseType::ExtensionFailure as u8,
+            )),
+        }
+    }
+
+    fn openssh_com_session_bind(&mut self, r: &mut DataReader) -> Result<MessageBuilder> {
+        let key = r.get_slice()?;
+        let sid = r.get_slice()?;
+        let sig = r.get_slice()?;
+        let fwd = r.get_bool()?;
+        Ok(MessageBuilder::new(
+            SshAgentResponseType::ExtensionFailure as u8,
+        ))
+    }
+
+    fn unhandled(&mut self, _: &[u8]) -> Result<MessageBuilder> {
         Ok(MessageBuilder::failure())
     }
 
     fn remove_identity(&mut self, data: &[u8]) -> Result<MessageBuilder> {
-        let mut reader = DataReader::with_pos(1, data);
+        let mut reader = DataReader::new(data);
+        reader.skip_u8()?;
         let key = reader.get_slice()?;
         let i = self.find_position(key)?;
         self.identities.remove(i);
@@ -290,25 +330,25 @@ impl SshAgent {
     }
 
     fn lock(&mut self, data: &[u8]) -> Result<MessageBuilder> {
-        let mut reader = DataReader::with_pos(1, data);
-        let secret =  reader.get_slice()?;
+        let mut reader = DataReader::new(data);
+        reader.skip_u8()?;
+        let secret = reader.get_slice()?;
         self.secret.zeroize();
         self.secret.0.clear();
         self.secret.0.extend_from_slice(secret);
         self.lock_status = LockStatus::Locked;
         self.encrypt_store();
-        Ok(MessageBuilder::new(SshAgentResponseType::Success as u8))
+        Ok(MessageBuilder::success())
     }
 
-    fn encrypt_store(&mut self) {
-    }
+    fn encrypt_store(&mut self) {}
 
-    fn decrypt_store(&mut self) {
-    }
+    fn decrypt_store(&mut self) {}
 
-    fn unlock(&mut self,  data: &[u8]) -> Result<MessageBuilder> {
-        let mut reader = DataReader::with_pos(1, data);
-        let secret =  reader.get_slice()?;
+    fn unlock(&mut self, data: &[u8]) -> Result<MessageBuilder> {
+        let mut reader = DataReader::new(data);
+        reader.skip_u8()?;
+        let secret = reader.get_slice()?;
         Ok(if secret != self.secret.0 {
             MessageBuilder::failure()
         } else {
